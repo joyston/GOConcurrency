5dc262fbe884592dceaa524351708619eaa9d9dc
LAPTOP-A5M7S5QL\joyst <joystonf@gmail.com>
2022-07-10 12:36:17 +0530

    -added code for buffered channels -added fibonacci function to send values over the channel and to understand the concept of closing a channel -used range on the channel to receive values sent  by the fibonacci goroutine -used capacity of the channel

--- main.go
+++ main.go
@@ -6,6 +6,15 @@ func swap(x int, c chan int) {
 	c <- x * 2
 }
 
+func fibonacci(n int, c chan int) {
+	x, y := 0, 1
+	for i := x; i < n; i++ {
+		c <- x
+		x, y = y, x+y
+	}
+	close(c)
+}
+
 func main() {
 	c := make(chan int)
 	go swap(2, c)
@@ -13,4 +22,10 @@ func main() {
 	y := <-c
 	x := <-c
 	fmt.Println(y, x)
+
+	ch := make(chan int, 10)
+	go fibonacci(cap(ch), ch)
+	for i := range ch {
+		fmt.Println(i)
+	}
 }
