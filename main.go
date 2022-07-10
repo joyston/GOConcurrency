aa7bee7b7d6c3b7c663d0d0189cdb25179f25f36
LAPTOP-A5M7S5QL\joyst <joystonf@gmail.com>
2022-07-10 14:19:06 +0530

    - used select statement to wait on multiple communication operations

--- main.go
+++ main.go
@@ -15,6 +15,19 @@ func fibonacci(n int, c chan int) {
 	close(c)
 }
 
+func selectFibonacci(c, quit chan int) {
+	a, b := 0, 1
+	for {
+		select {
+		case c <- a:
+			a, b = b, a+b
+		case <-quit:
+			fmt.Println("Quit")
+			return
+		}
+	}
+}
+
 func main() {
 	c := make(chan int)
 	go swap(2, c)
@@ -25,7 +38,19 @@ func main() {
 
 	ch := make(chan int, 10)
 	go fibonacci(cap(ch), ch)
+
 	for i := range ch {
 		fmt.Println(i)
 	}
+
+	cValues := make(chan int)
+	cQuit := make(chan int)
+	fmt.Println("********Fibonacci using Select*****")
+	go func() {
+		for j := 0; j < 10; j++ {
+			fmt.Println(<-cValues)
+		}
+		cQuit <- 0
+	}()
+	selectFibonacci(cValues, cQuit)
 }
